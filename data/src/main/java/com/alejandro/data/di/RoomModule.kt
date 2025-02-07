ef390730c4c6bfc59e3b9a26d450cd33603dd211
Alejandro <a98z9883@gmail.com>
2025-02-07 16:23:15 -0500

    feat: setup room  for data persistence

---
+++ data/src/main/java/com/alejandro/data/di/RoomModule.kt
@@ -0,0 +1,28 @@
+package com.alejandro.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.alejandro.data.local.room.AppDataBase
+import com.alejandro.data.local.room.dao.AccountDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+    private const val DATA_BASE_NAME = "challengeDb"
+
+    @Singleton
+    @Provides
+    fun providesRoom(@ApplicationContext context: Context): AppDataBase {
+        return Room.databaseBuilder(context, AppDataBase::class.java, DATA_BASE_NAME).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesTokenDao(db: AppDataBase): AccountDao = db.accountDao()
+}
---
+++ data/src/main/java/com/alejandro/data/local/room/dao/AccountDao.kt
@@ -0,0 +1,19 @@
+package com.alejandro.data.local.room.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.alejandro.data.local.room.entity.AccountDb
+
+@Dao
+interface AccountDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAccount(accountDb: AccountDb)
+
+    @Query("select  * from account where account = :accountNumber limit 1")
+    suspend fun getAccountByAccountNumber(accountNumber: String): AccountDb?
+
+    @Query("delete from account")
+    suspend fun deleteAllAccount()
+}
