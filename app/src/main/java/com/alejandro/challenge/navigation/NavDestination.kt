ced1b9cb6b70bdb989d65aaa1390ffbb3f1796f0
Alejandro <a98z9883@gmail.com>
2025-02-08 02:02:41 -0500

    feat: Implement navigation for login

---
+++ app/src/main/java/com/alejandro/challenge/navigation/NavDestination.kt
@@ -0,0 +1,12 @@
+package com.alejandro.challenge.navigation
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * Auth
+ */
+
+@Serializable
+@SerialName("LoginDriverDestination")
+data object LoginDestination
