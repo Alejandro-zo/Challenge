c210054e044e6409f399de35a4dc5e45a8e5d44a
Alejandro <a98z9883@gmail.com>
2025-02-09 18:03:56 -0500

    feat: Implement domain layer for product detail

--- domain/src/main/java/com/alejandro/domain/repository/AccountRepository.kt
+++ domain/src/main/java/com/alejandro/domain/repository/AccountRepository.kt
@@ -1,11 +1,16 @@
 package com.alejandro.domain.repository
 
 import com.alejandro.domain.entity.Account
+import com.alejandro.domain.entity.Movement
 
 interface AccountRepository {
 
     suspend fun getAccount() : List<Account>
 
     suspend fun updateAccount() : List<Account>
+
+    suspend fun getAccountByAccountNumber(accountNumber: String) : Account
+
+    suspend fun getMovements(accountNumber: String) : List<Movement>
 
 }
